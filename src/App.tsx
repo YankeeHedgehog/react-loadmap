8bf5c3934a9e151248f15b46b9b2912a783413cf
YankeeHedgehog <yankee.hedgehog.610@gmail.com>
2023-07-27 19:40:43 +0900

    add: Add new sample

--- src/App.tsx
+++ src/App.tsx
@@ -8,21 +8,31 @@ import Application from './features/application/Application'
 import Todos from './features/todos/Todos'
 import Timer from './features/timer/Timer'
 import Timer2 from './features/timer/Timer2'
+import Form from './form/Form'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8080/v1/graphql',
+  cache: new InMemoryCache(),
+})
 
 function App() {
   return (
-    <Routes>
-      <Route path="" element={<Index />}>
-        <Route path="" element={<Applications />} />
-        <Route path="" element={<Application />}>
-          <Route path="counter" element={<Counter />} />
-          <Route path="todos" element={<Todos />} />
-          <Route path="timer" element={<Timer />} />
-          <Route path="timer2" element={<Timer2 />} />
+    <ApolloProvider client={client}>
+      <Routes>
+        <Route path="" element={<Index />}>
+          <Route path="" element={<Applications />} />
+          <Route path="" element={<Application />}>
+            <Route path="counter" element={<Counter />} />
+            <Route path="todos" element={<Todos />} />
+            <Route path="timer" element={<Timer />} />
+            <Route path="timer2" element={<Timer2 />} />
+            <Route path="form" element={<Form />} />
+          </Route>
         </Route>
-      </Route>
-      <Route path="/sidebar" element={<SidebarIndex />} />
-    </Routes>
+        <Route path="/sidebar" element={<SidebarIndex />} />
+      </Routes>
+    </ApolloProvider>
   )
 }
 
